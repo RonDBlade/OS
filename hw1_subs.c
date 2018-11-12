a4a559e3f913e159d4bc4cd6f7427d111c4232ca
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2018-11-12 20:19:27 +0200

    Update hw1_subs.c

--- hw1_subs.c
+++ hw1_subs.c
@@ -41,6 +41,7 @@ int main(int argc,char** argv){
 	char* tempchar=(char*)calloc(2,1);
 	if(tempchar==NULL){
 		printf("Error: calloc has failed (memory allocation error)\n");
+		free(buffer);
 		return 1;
 	}
 	char* dir=getenv("HW1DIR");
