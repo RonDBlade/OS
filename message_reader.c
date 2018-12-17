859a5135cc0cb99ddc0b3d458f8a1cf2a3fbc401
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2018-12-17 18:20:55 +0200

    Update message_reader.c

--- message_reader.c
+++ message_reader.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <errno.h>
 
-int string_to_int(char* number){
+int string_to_int(char* number){/*make the channel arg to an actual int and not string*/
 	int i,sum=0;
 	for(i=0;i<strlen(number);i++){
 		sum*=10;
@@ -18,7 +18,7 @@ int string_to_int(char* number){
 
 int main(int argc,char** argv){
 	int fd,val,channel,i;
-	char buffer[129];
+	char buffer[129];/*max size of message in channel+1 for '\0'*/
 	for(i=0;i<129;i++){/*zero out the output array*/
 		buffer[i]='\0';
 	}
@@ -26,24 +26,23 @@ int main(int argc,char** argv){
 		printf("Not the right amount of arguments passed for the reader\n");
 		return -1;
 	}
-	fd=open(argv[1],O_RDWR);
+	fd=open(argv[1],O_RDWR);/*open*/
 	if(fd<0){
 		printf("Error opening file: %s\n",strerror(errno));
 		return -1;
 	}
 	channel=string_to_int(argv[2]);
-	val=ioctl(fd,MSG_SLOT_CHANNEL,channel);
+	val=ioctl(fd,MSG_SLOT_CHANNEL,channel);/*change the channel*/
 	if(val<0){
 		printf("Error changing channel: %s\n",strerror(errno));
 		return -1;
 	}
-	val=read(fd,buffer,128);
+	val=read(fd,buffer,128);/*read*/
 	if(val<0){
 		printf("Error reading from channel: %s\n",strerror(errno));
 		return -1;
 	}
-	close(fd);
+	close(fd);/*close*/
 	printf("Read successful from %s channel %d\n",argv[1],channel);
-	printf("message was: %s\n",buffer);
 	return 0;
 }
