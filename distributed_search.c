366e06bb5432ddced270766cd4abe64cdcc8caa7
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2018-12-29 18:07:38 +0200

    Add files via upload

--- distributed_search.c
+++ distributed_search.c
@@ -4,14 +4,19 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <errno.h>
 
-typedef struct queueStruct{
+typedef struct queueStruct{/*queue struct and operations from geeksforgeeks.org*/
 	int size,front,rear;
 	char* array;
 }queueStruct;
 
 queueStruct* initQueue(){
 	queueStruct* queue=(queueStruct*) malloc(sizeof(queueStruct));
+	if(queue==NULL){
+		printf("malloc failed: %s\n",strerror(errno));
+		exit(1);
+	}
 	queue->size=0;
 	queue->front=0;
 	queue->rear=0;
@@ -21,6 +26,8 @@ queueStruct* initQueue(){
 
 void enqueue(queueStruct* queue,char* item){/*inserts new item to the queue*/
 	int i;
+	queue->array=(char*)realloc(queue->array,sizeof(char)*(queue->size)+1);
+	/*add check if it fails*/
 	for(i=0;i<NAME_MAX;i++){/*insert item to the last place in the queue*/
 		if(i<strlen(item))
 			queue->array[(queue->rear)+i]=item[i];
@@ -40,48 +47,88 @@ void dequeue(queueStruct* queue,char* item){/*pops out the first item in the que
 	queue->size=(queue->size)-1;
 }
 
-static int count;
+static long count=0;
 static char* name;
 static queueStruct* current_dirs;/*max size of name of dir is NAME_MAX (from limits.h)*/
+pthread_mutex_t lock;
 
 int string_to_int(char* thread_count){/*translate the argument which specifies how many threads we want to use to an int*/
-	int count=0,i;
+	long count=0,i;
 	for(i=0;i<strlen(thread_count);i++){
 		count*=10;
-		count+=thread_count[i];
+		count+=thread_count[i]-'0';
 	}
 	return count;
 }
 
-void* thread_func(void* thread_param){
+void* thread_func(void* thread_param){/*what each thread does*/
+	/*listing items in directory from stackoverflow "how to list files in a directory"*/
+	int rc;
+	long num=(long)thread_param;
 	DIR *d;
 	struct dirent *dir;
 	char dirname[128];
 	dequeue(current_dirs,dirname);
 	d=opendir((const char*)dirname);
-	exit(0);
+	if(d){
+		while((dir=readdir(d))!=NULL){
+			if((dir->d_name)!="." || (dir->d_name)!="..")
+				continue;
+			if(dir->d_type==DT_DIR)
+				enqueue(current_dirs,dir->d_name);
+			if(dir->d_name==name)
+				rc=pthread_mutex_lock(&lock);
+				if(rc){
+					printf("ERROR in pthread_mutex_lock(): %s\n",strerror(rc));
+					exit(1);
+				}
+				count++;
+				rc=pthread_mutex_unlock(&lock);
+				if(rc){
+					printf("ERROR in pthread_mutex_unlock(): %s\n",strerror(rc));
+					exit(1);
+				}
+		}
+	}
+	closedir(d);
+
+
+
+	pthread_exit(NULL);
 }
 
 int main(int argc,char** argv){
-	int num_threads,i,rc;
+	long num_threads,i;
+	int rc;
+	void* status;
 	name=argv[2];
 	current_dirs=initQueue();
+	enqueue(current_dirs,arv[1]);
+	rc=pthread_mutex_init(&lock,NULL);
+	if(rc){
+		printf("ERROR in pthread_mutex_init(): %s\n",strerror(rc));
+		exit(1);
+	}
 	num_threads=string_to_int(argv[3]);
+	printf("number of threads in total is %ld\n",num_threads);
 	pthread_t thread[num_threads];
 	for(i=0;i<num_threads;i++){
-		rc=pthread_create(&thread[i],NULL,thread_func,NULL);
+		rc=pthread_create(&thread[i],NULL,thread_func,(void*)i);
 		if(rc){
 			printf("ERROR in pthread_create(): %s\n",strerror(rc));
 			exit(1);
 		}
-
-
 	}
-
-
-
-
-
-
+	for(i=0;i<num_threads;i++){
+		rc=pthread_join(thread[i],&status);
+		if(rc){
+			printf("ERROR in pthread_join(): %s\n",strerror(rc));
+			exit(1);
+		}
+		printf("Main: completed join with thread %ld having a status of %ld\n",i,(long)status);
+	}
+	printf("amount of threads was %ld\n",count);
+	free(current_dirs->array);
+	free(current_dirs);
 	exit(0);
 }
